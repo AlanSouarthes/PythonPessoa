8f3fe76735fdeee1a54d0d0598ac1eaf15ef20fb
ALAN OLIVEIRA SOUARTHES <alan.osouarthes@senacsp.edu.br>
2023-10-30 17:05:46 -0300

    first commit

---
+++ ModelPessoa.py
@@ -0,0 +1,42 @@
+#Classe de Model Pessoa
+class ModelPessoa:
+    #Declaração de variaveis
+
+    def __init__(self): #Representa um método construtor
+        self.cpf = 0
+        self.nome = ""
+        self.telefone = 0
+        self.endereco = ""
+        self.email = ""
+
+    def cadastrarPessoa(self,cpf,nome,telefone,endereco,email):
+        self.cpf = cpf
+        self.nome = nome
+        self.telefone = telefone
+        self.endereco = endereco
+        self.email = email
+
+    def consultarPessoa(self):
+        consulta = f"CPF: {self.cpf}\nNome: {self.nome}\nTelefone: {self.telefone}\nEndereço: {self.endereco}\nE-mail: {self.email}"
+        return consulta
+
+
+    def atualizarPessoa(self,opcao,dado):
+        if opcao == 1:
+            self.nome = dado
+        elif opcao == 2:
+            self.telefone = dado
+        elif opcao == 3:
+            self.endereco == dado
+        elif opcao == 4:
+            self.email == dado
+
+
+    def excluirPessoa(self):
+        self.cpf = 0
+        self.nome = ""
+        self.telefone = 0
+        self.endereco = ""
+        self.email = ""
+
+
